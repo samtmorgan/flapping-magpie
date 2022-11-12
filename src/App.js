e870af9df48a474da3632619b6d39f008a56b25d
samumo <sambola@gmail.com>
2022-11-12 07:10:56 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+const words = [
+  "Halloween",
+  "April",
+  "Trindade",
+  "Hideous",
+  "Rotten",
+  "Decaying",
+  "guilty",
+  "lonely",
+  "heartbroken",
+  "overwhelmed",
+  "potion",
+  "devastated",
+  "through",
+  "experiment",
+  "favourite",
+  "heard",
+  "peculiar",
+  "scent",
+  "strange",
+  "special",
+  "mysteriously",
+  "menacingly",
+  "independent",
+  "incomplete",
+  "disapprove",
+  "dishonest",
+  "disappear",
+  "disagree",
+  "straight",
+  "castle",
+  "eerily",
+  "incapable",
+  "acrobats",
+  "jugglers",
+  "clowns",
+  "circus",
+  "travel",
+  "travelling",
+  "performer",
+  "ticket",
+  "even though",
+  "because",
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>{/* Edit <code>src/App.js</code> and save to reload. */}</p>
+        {words.map((word, index) => (
+          <div>
+            {index + 1}. {word}
+          </div>
+        ))}
         <a
           className="App-link"
           href="https://reactjs.org"
