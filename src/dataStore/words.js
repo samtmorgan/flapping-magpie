b78cdd6884c2a1340a2b8532e7f337f0a8ef4442
samumo <sambola@gmail.com>
2022-11-12 07:49:16 +0000

    added folder for data and started prettier insatal

---
+++ src/dataStore/words.js
@@ -0,0 +1,54 @@
+let words = [
+  // 14/10/2022
+  [
+    "dangerous",
+    "mountainous",
+    "famous",
+    "enormous",
+    "jealous",
+    "tremendous",
+    "venomous",
+    "poisonous",
+    "perilous",
+    "generous",
+  ],
+  //   21/11/2022
+  [
+    "acrobats",
+    "jugglers",
+    "clowns",
+    "circus",
+    "travel",
+    "travelling",
+    "performer",
+    "ticket",
+    "even though",
+    "because",
+  ],
+  // 04/11/2022
+  [
+    "which",
+    "whilst",
+    "however",
+    "although",
+    "despite",
+    "fairly",
+    "treat",
+    "actually",
+    "important",
+    "enough",
+  ],
+  // 11/11/2022
+  [
+    "people",
+    "say",
+    "other",
+    "addition",
+    "many",
+    "some",
+    "furthermore",
+    "conclusion",
+    "entertainment",
+    "moral",
+  ],
+];
